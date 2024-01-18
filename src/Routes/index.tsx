e589a4b435ac167993b526b8b1edba351f8496e4
NaginB <naginbanodha@gmail.com>
2024-01-18 14:06:56 +0530

    updated code

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import * as R from './styles';
-import Home from '../Pages/Home';
-import NotFound from '../Pages/NotFound';
-import Sidebar from '../Components/Sidebar';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as R from "./styles";
+import Home from "../Pages/Home";
+import NotFound from "../Pages/NotFound";
+import Sidebar from "../Components/Sidebar";
 
 interface RoutesInterface {
-    element: React.FC,
-    path: string,
-    private?: boolean,
-    sidebar?: boolean
-    header?: boolean
+  element: React.FC;
+  path: string;
+  private?: boolean;
+  sidebar?: boolean;
+  header?: boolean;
 }
 
 const AppRoutes: React.FC = () => {
-    const [routes] = useState<RoutesInterface[]>([
-        {
-            element: Home,
-            path: '/',
-            sidebar: true
-        },
-        {
-            element: NotFound,
-            path: '*'
-        }
-    ]);
+  const [routes] = useState<RoutesInterface[]>([
+    {
+      element: Home,
+      path: "/dashboard",
+      sidebar: true,
+    },
+    {
+      element: NotFound,
+      path: "*",
+    },
+  ]);
 
-    return (
-        <Router>
-            <Routes>
-                {
-                    routes.map(({ element: Element, path, sidebar }) => (
-                        <Route element={
-                            <R.RoutesWrapper>
-                                {sidebar && <Sidebar />}
-                                <R.Content>
-                                    <Element />
-                                </R.Content>
-                            </R.RoutesWrapper>
-                        } path={path} />
-                    ))
-                }
-            </Routes>
-
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        {routes.map(({ element: Element, path, sidebar }) => (
+          <Route
+            element={
+              <R.RoutesWrapper>
+                {sidebar && <Sidebar />}
+                <R.Content>
+                  <Element />
+                </R.Content>
+              </R.RoutesWrapper>
+            }
+            path={path}
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRoutes;
