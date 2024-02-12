6cbfc0dc154c6f1cf9ae633e90ca1b41d445a573
NaginB <naginbanodha@gmail.com>
2024-02-12 15:23:33 +0530

    updated code 12 Feb

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -5,55 +5,71 @@ import { Link } from "react-router-dom";
 import { TbEyeClosed } from "react-icons/tb";
 import ColoredLogo from "../../Assets/Images/logo-colored.svg";
 import GoogleIcon from "../../Assets/Images/google-icon.svg";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from '@react-oauth/google';
 
 const Login: React.FC = () => {
+  const CLIENT_ID: string = process.env.REACT_APP_CLIENT_ID! || '';
   return (
-    <S.LoginWrapper>
-      <S.LoginContainer>
-        <S.ColoredLogo src={ColoredLogo} />
-        <S.LoginContent>
-          <h1 className="font-roboto font-reg">Log in</h1>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <S.LoginWrapper>
+        <S.LoginContainer>
+          <S.ColoredLogo src={ColoredLogo} />
+          <S.LoginContent>
+            <h1 className="font-roboto font-reg">Log in</h1>
 
-          <div className="login-form">
-            <div>
-              <C.CommonInput placeholder="Email " />
-            </div>
-            <C.IconInputWrapper>
-              <C.CommonInput placeholder="Password " />
-              <TbEyeClosed className="input-icon" size={18} />
-            </C.IconInputWrapper>
+            <div className="login-form">
+              <div>
+                <C.CommonInput placeholder="Email " />
+              </div>
+              <C.IconInputWrapper>
+                <C.CommonInput placeholder="Password " />
+                <TbEyeClosed className="input-icon" size={18} />
+              </C.IconInputWrapper>
 
-            <C.CommonButton>Continue</C.CommonButton>
-            <div className="text-center">
-              <Link to="/password/forget" className="text-white">
-                Forgot Password ?
-              </Link>
-            </div>
-
-            <div className="or-title">
-              <span>Or</span>
-            </div>
-
-            <div className="text-center">
-              <p className="text-[#767676]">
-                Don't have an account ?
-                <Link className="text-white" to={"/signup"}>
-                  {" "}
-                  Sign up
+              <C.CommonButton>Continue</C.CommonButton>
+              <div className="text-center">
+                <Link to="/password/forget" className="text-white">
+                  Forgot Password ?
                 </Link>
-              </p>
-            </div>
+              </div>
 
-            <div>
-              <C.CommonIonButton>
-                <img alt="fanxo-logo" src={GoogleIcon} />
-                <span>Login With Google</span>
-              </C.CommonIonButton>
+              <div className="or-title">
+                <span>Or</span>
+              </div>
+
+              <div className="text-center">
+                <p className="text-[#767676]">
+                  Don't have an account ?
+                  <Link className="text-white" to={"/signup"}>
+                    {" "}
+                    Sign up
+                  </Link>
+                </p>
+              </div>
+
+              <div>
+
+                <GoogleLogin
+                  onSuccess={credentialResponse => {
+                    console.log(credentialResponse);
+                  }}
+                  onError={() => {
+                    console.log('Login Failed');
+                  }}
+                  useOneTap
+                />;
+
+                <C.CommonIonButton>
+                  <img alt="fanxo-logo" src={GoogleIcon} />
+                  <span>Login With Google</span>
+                </C.CommonIonButton>
+              </div>
             </div>
-          </div>
-        </S.LoginContent>
-      </S.LoginContainer>
-    </S.LoginWrapper>
+          </S.LoginContent>
+        </S.LoginContainer>
+      </S.LoginWrapper>
+    </GoogleOAuthProvider>
   );
 };
 
--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import userSlice from './slices/userSlice'
 
 export const store = configureStore({
   reducer: {
+    user: userSlice
     // posts: postsReducer,
     // comments: commentsReducer,
     // users: usersReducer,
